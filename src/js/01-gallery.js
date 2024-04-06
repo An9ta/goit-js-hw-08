0bb42323191ebed5c2a6b540217e8d1c92c934f3
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-06 23:38:58 +0200

    4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,20 +7,24 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 
-//const gallery = document.querySelector(".gallery");
-//const items = galleryItems.map(({ original, preview, description }) => {
-//const galleryLink = document.createElement("a");
-//galleryLink.className = "gallery__link";
-//galleryLink.href = original;
+const gallery = document.querySelector('.gallery');
+const items = galleryItems.map(({ original, preview, description }) => {
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = original;
 
-//const galleryImage = document.createElement("img");
-//galleryImage.className = "gallery__image";
-//galleryImage.src = preview;
-//galleryImage.setAttribute("data-source", original);
-//galleryImage.setAttribute("title", description);
-//galleryImage.alt = description;
-//galleryLink.append(galleryImage);
-//return galleryLink;
-//});
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = preview;
+  galleryImage.setAttribute('data-source', original);
+  galleryImage.setAttribute('title', description);
+  galleryImage.alt = description;
+  galleryLink.append(galleryImage);
+  return galleryLink;
+});
 
-//gallery.append(...items);
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
