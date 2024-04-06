a2bae712525a87f41ebde15c0082557409e3e3dc
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-06 21:44:37 +0200

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+//const gallery = document.querySelector(".gallery");
+//const items = galleryItems.map(({ original, preview, description }) => {
+//const galleryLink = document.createElement("a");
+//galleryLink.className = "gallery__link";
+//galleryLink.href = original;
+
+//const galleryImage = document.createElement("img");
+//galleryImage.className = "gallery__image";
+//galleryImage.src = preview;
+//galleryImage.setAttribute("data-source", original);
+//galleryImage.setAttribute("title", description);
+//galleryImage.alt = description;
+//galleryLink.append(galleryImage);
+//return galleryLink;
+//});
+
+//gallery.append(...items);
