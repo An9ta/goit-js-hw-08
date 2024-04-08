22c69fe6be0239d62f4ca97f949e03589c3534eb
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-08 20:37:17 +0200

    9

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,4 +2,14 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+const iframePlayer = new Player(iframe);
+
+iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+iframePlayer.on('timeupdate', function (data) {
+  delayedTimeUpdate(data);
+});
+
+const delayedTimeUpdate = throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
