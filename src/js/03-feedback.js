8e6a947ad911ae6edff74ff433c3f174cacfdd59
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-08 23:40:27 +0200

    10

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,33 @@
 import throttle from 'lodash.throttle';
 
-//Śledź w formularzu zdarzenie input, i za każdym razem zapisuj w local storage obiekt z
-//polami email i message, w których przechowywane są aktualne wartości pól formularza.
-//Niech kluczem do tych danych w storage będzie "feedback-form-state".
+const form = document.querySelector('form.feedback-form');
 
-//Podczas przeładowywania strony sprawdzaj stan storage i jeśli są tam zapisane dane,
-//wypełniaj nimi pola formularza.W przeciwnym wypadku pola powinny być puste.
+const formDataString = localStorage.getItem('feedback-form-state');
+if (formDataString) {
+  const formData = JSON.parse(formDataString);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
 
-//Po wysłaniu formularza wyczyść storage i pola formularza, a także wyloguj obiekt z
-//polami email, message i ich aktualnymi wartościami do konsoli.
+form.addEventListener('input', e => {
+  storedData();
+});
 
-//Zrób tak, aby storage aktualizował się nie częściej niż raz na 500 milisekund.
-//Aby to zrobić, użyj metody biblioteki lodash.throttle (dodaj ją do projektu).
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  console.log(data);
+  localStorage.clear();
+  form.reset();
+});
+
+const storedData = throttle(() => {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
