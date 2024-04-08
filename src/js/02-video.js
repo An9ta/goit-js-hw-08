93cc0c587c634cbe7a881dab75d0e3038e0d4908
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-08 17:09:34 +0200

    8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,18 +3,3 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Vimeo.Player(iframe);
-
-//Zapisuj czas odtwarzania w local storage. Niech kluczem w storage będzie "videoplayer-current-time".
-//Przy przeładowywania strony używaj metody setCurrentTime() aby wznowić odtwarzanie od zapisanego
-//momentu.
-iframePlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-//Zbadaj dokumentację metody on() i zacznij śledzić zdarzenie timeupdate - czyli aktualizacje czasu
-//odtwarzania.
-iframePlayer.on('timeupdate', function (data) {
-  delayedTimeUpdate(data);
-});
-//Dodaj do projektu bibliotekę lodash.throttle i zrób tak, aby czas odtwarzania aktualizował
-//się w storage nie częściej niż raz na sekundę.
-const delayedTimeUpdate = throttle(function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000);
